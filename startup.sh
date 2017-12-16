bca404647ab63fc4751550c82559c9b23d9cbc01
Neil Newbold <neil.newbold@gmail.com>
2017-12-16 17:34:03 +0000

    Fixed script permissions

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 docker pull psiinon/bodgeit
 docker network create security-tests
-run --rm --net security-tests --name webapp -p 50000:8080 -i -t psiinon/bodgeit'
+docker run --rm --net security-tests --name webapp -p 50000:8080 psiinon/bodgeit
