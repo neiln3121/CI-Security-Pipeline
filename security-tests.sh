bad0e510d24c0dbc6442169873a57077817e2ca0
Neil Newbold <neil.newbold@gmail.com>
2017-12-17 02:56:29 +0000

    Experiment

--- security-tests.sh
+++ security-tests.sh
@@ -2,15 +2,14 @@ ZAP_PORT=8090
 echo "::running security tests"
 rm -rf $PWD/security
 rm -rf $PWD/artifacts
-mkdir -p $PWD/security $PWD/artifacts;
+mkdir -m775 -p $PWD/security $PWD/artifacts;
 
 echo "::running zap tests"
 docker pull owasp/zap2docker-weekly
 echo ":::Baseline scan"
-docker run -t --name zap -u zap --link webapp -v /tmp:/zap/wrk:rw owasp/zap2docker-weekly zap-baseline.py \
+docker run --rm -t --name zap --link webapp -v $PWD/security:/zap/wrk:rw owasp/zap2docker-weekly zap-baseline.py \
     -t http://webapp:8080/bodgeit -g gen.conf -r zap-report.html
-docker cp zap:/zap/wrk/zap-report.html $PWD/artifacts;
-docker rm zap
+mv $PWD/security/zap-report.html $PWD/artifacts;
 
 echo "::running arachni tests"
 docker pull ahannigan/docker-arachni
@@ -20,12 +19,10 @@ docker run --rm \
     bin/arachni http://webapp:8080/bodgeit \
     --browser-cluster-pool-size=1 \
     --report-save-path=reports/result.io.afr;
-docker run \
+docker run --rm \
     --name=arachni_report  \
-    -v /tmp:/arachni/reports ahannigan/docker-arachni \
+    -v $PWD/security:/arachni/reports ahannigan/docker-arachni \
     bin/arachni_reporter reports/result.io.afr \
     --reporter=html:outfile=reports/arachni-report.html.zip;
-docker cp arachni_report:/arachni/reports/arachni-report.html.zip $PWD/security;
 unzip $PWD/security/arachni-report.html.zip -d $PWD/artifacts
-docker rm arachni_report
 
